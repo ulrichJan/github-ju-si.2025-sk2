4c197b297f6a29ce2c15d2963696ee2aa55bec98
Jan Ulrich <jan.ulrich@tul.cz>
2025-10-24 20:41:52 +0200

    #6 Domácí úkol- pravoúhlý trojúhelník

---
+++ IS-Programy/program004b-pravouhly-trojuhelnik/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Zadej výšku trojúhelníku: ");
+        int vyska = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= vyska; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
